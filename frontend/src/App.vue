ba6d38d15e56cba3387b75983bc5cbc15b9dc1c6
HayatoTomita <nice.to.tom@gmail.com>
2022-01-29 23:59:20 +0900

    とりあえずツイートのカードをコンポーネントとして登録

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <v-app>
     <v-main>
+      <tweetcard></tweetcard>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import tweetcard from "./components/tweetcard";
 export default {
   name: 'App',
-
+  components: { tweetcard },
   data: () => ({
     //
   }),
---
+++ frontend/src/components/tweetcard.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card
+    class="mx-auto"
+    color="#26c6da"
+    dark
+    max-width="400"
+  >
+    <v-card-text class="text-h5 font-weight-bold">
+      "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
+    </v-card-text>
+
+    <v-card-actions>
+      <v-list-item class="grow">
+        <v-img
+        class="elevation-6"
+        alt=""
+        src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light">
+        </v-img>
+      </v-list-item>
+    </v-card-actions>
+  </v-card>
+</template>
